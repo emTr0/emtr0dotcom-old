7590427f6417d50425a936e90f432609675d319b
emTr0 <emtr0@emtr0.com>
2017-04-20 11:17:16 -0700

    Updating some files.

--- custom_storages.py
+++ custom_storages.py
@@ -1,8 +1,13 @@
+# custom_storages.py
 from django.conf import settings
 from storages.backends.s3boto import S3BotoStorage
 
-class StaticStorage(S3BotoStorage):
+from django.core.files.storage import get_storage_class
+from filebrowser_safe.storage import S3BotoStorageMixin
+
+class StaticStorage(S3BotoStorage, S3BotoStorageMixin):
     location = settings.STATICFILES_LOCATION
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3BotoStorage, S3BotoStorageMixin):
     location = settings.MEDIAFILES_LOCATION
+
